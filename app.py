6da764cf835b76ee758bd7506e50f3f920381480
veranyagaka <nyagakaver@gmail.com>
2024-10-01 02:28:22 +0300

    WTForms

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+# wtforms
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sk'
+class MyForm(FlaskForm):
+	name = StringField('Name', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+	form = MyForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		return f"Hello {name}"
+	return render_template('form.html', form=form)
 @app.route('/')
 def home():
 	return "Hello Flask"
