dccc155e8aef3afe90e444d7466e199ce889a655
veranyagaka <nyagakaver@gmail.com>
2024-10-01 03:09:00 +0300

    Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -3,12 +3,31 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
+# sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), unique=True, nullable=False)
+	def __repr__(self):
+		return f"User('{self.name}')"
 class MyForm(FlaskForm):
 	name = StringField('Name', validators=[DataRequired()])
 	submit = SubmitField('Submit')
+@app.route('/add_user/<name>')
+def add_user(name):
+	user = User(name=name)
+	db.session.add(user)
+	db.session.commit()
+	return f"User {name} added"
+@app.route('/users')
+def users():
+    users = User.query.all()
+    return str(users)
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
@@ -31,3 +50,16 @@ def contact():
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+'''
+from app import db, app
+
+# Use parentheses to call app_context()
+with app.app_context():  
+    db.create_all()  # Create all tables
+
+python
+
+sqlite3 mydatabase.db
+.tables
+'''
